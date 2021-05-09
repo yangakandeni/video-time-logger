25ba9f063bf8e76f58fcff38ffc0f7b7f65fecd3
yangakandeni <kandeni.yanga>
2021-05-09 17:21:55 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Video Time Logger (Console App)
+
+## This is a python program for a video time logger (console) app
+
+### RUN PROGRAM
+
+```bash
+python task_1.py
+```
+
+```bash
+python task_2.py
+```
+
+![video-time-logger-instructions](/screenshots/video-time-logger-assignment.png)
+
+## TESTS
+
+`The following tests have interactive prompts. Please enter the number 3, for test to pass` :see_no_evil:
+
+```bash
+python -m unittest tests/test_functions.py -v
+```
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,33 +5,30 @@ from functions import get_total_vidoes, record_running_times, log_running_times,
 
 class FunctionsTestCase(unittest.TestCase):
 
-    def setUp(self):
-    #     # get total videos
-        # self.total_videos = get_total_vidoes()
-    #     # self.records = record_running_times(3)
-    #     # self.logs = log_running_times(self.records)
-        return super().setUp()
+    def test_can_total_videos_returns_integer(self):
+        # enter 3 for test to pass
+        videos = get_total_vidoes()
 
-    # def test_can_total_videos_returns_integer(self):
-    #     # enter 3 for test to pass
-    #     videos = get_total_vidoes()
+        self.assertIsInstance(videos, int)
+        self.assertEqual(videos, 3)
 
-    #     self.assertIsInstance(videos, int)
-    #     self.assertEqual(videos, 3)
+    def test_display_error_on_invalid_input(self):
+        # enter any value that is not an integer
+        # I tried to do, with self.assertRaises() ...
 
-    # def test_display_error_on_invalid_input(self):
-    #     videos = get_total_vidoes()
-    #     self.assertEqual(videos, f"\nPlease enter a valid number\n")
+        videos = get_total_vidoes()
+        self.assertEqual(videos, f"\nPlease enter a valid number\n")
 
-    # def test_record_running_times_function(self):
-    #     # enter 3 for tests to pass
-    #     videos = get_total_vidoes()
-    #     records = record_running_times(videos)
-    #     self.assertEqual(len(records), 3)
+    def test_record_running_times_function(self):
+        # enter 3 for tests to pass
+        videos = get_total_vidoes()
+        records = record_running_times(videos)
+        self.assertEqual(len(records), 3)
 
     def test_log_running_times_function(self):
+        # enter 3 for tests to pass
         videos = get_total_vidoes()
-        records = record_running_times(3)
+        records = record_running_times(videos)
         pickled_data = log_running_times(records)
 
         self.assertDictEqual(records, pickled_data)
@@ -41,6 +38,3 @@ class FunctionsTestCase(unittest.TestCase):
         records = record_running_times(videos)
         result = calc_total_duration(records)
         self.assertTrue(isinstance(result, int))
-
-    # def test_display_running_times(self):
-
